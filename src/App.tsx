da41e9658fe095557bf1016c7367e57e5dce1976
Ray Everton <ray.evaf@gmail.com>
2021-05-06 21:51:18 -0300

    aplicando tema na aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React  from "react";
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
 
 import Layout from "./components/Layout";
+import dark from "./styles/theme/dark"
+
 const App: React.FC = () => {
 	return (
-    <>
+    <ThemeProvider theme={dark} >
       <GlobalStyle/>
       <Layout/>
-    </>
+    </ThemeProvider>
 		
 	);
 }
