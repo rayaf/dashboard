307f32eb5796a5f422e91f5819bd9549ec107803
Ray Everton <ray.evaf@gmail.com>
2021-05-09 11:53:32 -0300

    Aside estilizado

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
 
-import { Container } from "./styles";
+import { 
+  Container,
+  Header,
+  LogImg,
+  Title,
+  MenuContainer,
+  MenuItemLink
+} from "./styles";
+
+import { 
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp
+ } from "react-icons/md";
 
 const Aside: React.FC = () => {
+  const logoImg = require("../../assets/logo.svg").default as string;
+
 	return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogImg src={logoImg} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+        
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saidas
+        </MenuItemLink>
+        
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          sair
+        </MenuItemLink>
+        
+      </MenuContainer>
+
     </Container>
 	);
 }
