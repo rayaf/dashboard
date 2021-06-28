43fe6b0660bdc0319170d783cbe45e9952c93698
Ray Everton <ray.evaf@gmail.com>
2021-06-27 21:23:41 -0300

    add support to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import React  from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
 
-import Layout from "./components/Layout";
-import List from "./pages/List";
+import Routes from './routes';
 import dark from "./styles/theme/dark"
 
 const App: React.FC = () => {
 	return (
     <ThemeProvider theme={dark} >
       <GlobalStyle/>
-      <Layout>
-        <List />
-      </Layout>
+      <Routes />
     </ThemeProvider>
 		
 	);
